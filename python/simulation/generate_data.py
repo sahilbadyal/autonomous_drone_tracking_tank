aa565d6e1a522149159d552feee6764ac098b9b4
Sahil Badyal <sbadyals@gmail.com>
2020-04-29 22:07:40 -0700

    add env'

--- python/simulation/generate_data.py
+++ python/simulation/generate_data.py
@@ -8,13 +8,13 @@ import rospy
 os.environ["TFHUB_CACHE_DIR"] = '/home/local/ASUAD/sbadyal/Spring2020/autonomous_drone_tracking_tank/python/object_detection/model'
 
 def stop():
-    time.sleep(0.5)
+    #time.sleep(0.5)
     p = Popen(["rostopic","pub", "/joy", "sensor_msgs/Joy", "header:\n  seq: 0\n  stamp:\n    secs: 185\n    nsecs: 469000000\n  frame_id: ''\naxes: [-0.0, -0.0, -0.0, -0.0, 0.0, 0.0]\nbuttons: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]"], shell=False)
     time.sleep(1)
     p.kill()
 
 def move(x ,y, secs):
-    time.sleep(0.5)
+    #time.sleep(0.5)
     p = Popen(["rostopic","pub", "/joy", "sensor_msgs/Joy", "header:\n  seq: 0\n  stamp:\n    secs: 185\n    nsecs: 469000000\n  frame_id: ''\naxes: ["+str(x)+","+str(y)+", -0.0, -0.0, 0.0, 0.0]\nbuttons: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]"], shell=False)
     time.sleep(secs)
     p.kill()
@@ -32,12 +32,12 @@ print("Initialized Object Detector")
 
 def run():
     results = []
-    numberList1 = [x * 0.1 for x in range(-7, 8)]
+    numberList1 = [x * 0.1 for x in range(-4, 5)]
     #print(numberList1)
     x = random.choice(numberList1)
     y = random.choice(numberList1)
     order = abs(max([x,y], key=abs))
-    secs = 0.7
+    secs = 1
     print(order, secs)
     print("Aage")
     move(x, y, secs)

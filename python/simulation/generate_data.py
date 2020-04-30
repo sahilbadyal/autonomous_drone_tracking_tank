8c8e50d78fb6d8ca02e8148a99e9991a5097c01a
Sahil Badyal <sbadyals@gmail.com>
2020-04-29 20:19:38 -0700

    generate data improved

--- python/simulation/generate_data.py
+++ python/simulation/generate_data.py
@@ -3,15 +3,18 @@ import random
 import time
 from object_detection.object_detection import objectdetector
 import os
+import rospy
 
 os.environ["TFHUB_CACHE_DIR"] = '/home/local/ASUAD/sbadyal/Spring2020/autonomous_drone_tracking_tank/python/object_detection/model'
 
 def stop():
+    time.sleep(0.5)
     p = Popen(["rostopic","pub", "/joy", "sensor_msgs/Joy", "header:\n  seq: 0\n  stamp:\n    secs: 185\n    nsecs: 469000000\n  frame_id: ''\naxes: [-0.0, -0.0, -0.0, -0.0, 0.0, 0.0]\nbuttons: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]"], shell=False)
     time.sleep(1)
     p.kill()
 
 def move(x ,y, secs):
+    time.sleep(0.5)
     p = Popen(["rostopic","pub", "/joy", "sensor_msgs/Joy", "header:\n  seq: 0\n  stamp:\n    secs: 185\n    nsecs: 469000000\n  frame_id: ''\naxes: ["+str(x)+","+str(y)+", -0.0, -0.0, 0.0, 0.0]\nbuttons: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]"], shell=False)
     time.sleep(secs)
     p.kill()
@@ -29,31 +32,39 @@ print("Initialized Object Detector")
 
 def run():
     results = []
-    numberList1 = [x * 0.1 for x in range(-10, 10)]
-    print(numberList1)
+    numberList1 = [x * 0.1 for x in range(-7, 8)]
+    #print(numberList1)
     x = random.choice(numberList1)
     y = random.choice(numberList1)
     order = abs(max([x,y], key=abs))
-    secs = 1 
+    secs = 0.7
     print(order, secs)
+    print("Aage")
     move(x, y, secs)
     stop()
     save_image()
     results.append([str(x) for x in od.run('./data/foo.jpg')])
+    print("Peeche")
     move(-x, -y, secs)
     stop()
     save_image()
     results.append([str(x) for x in od.run('./data/foo.jpg')])
     return x, y, secs, results
 
-with open('dataSet.csv','w') as f:
-    for x in range(50):
+with open('dataSetlarge.csv','w') as f:
+    prevState = ['0.5','0.5']
+    for x in range(1000):
         x,y,s,r = run()
+        f.write(','.join(prevState))
+        f.write(',')
         f.write(','.join((str(x), str(y), str(s))))
         f.write(',')
         f.write(','.join(r[0]))
         f.write('\n')
+        f.write(','.join(r[0]))
+        f.write(',')
         f.write(','.join((str(-x), str(-y), str(s))))
         f.write(',')
         f.write(','.join(r[1]))
         f.write('\n')
+        prevState = r[1].copy()
